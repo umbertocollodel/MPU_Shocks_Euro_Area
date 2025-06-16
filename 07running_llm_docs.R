95054971b8746e33cbf48b086da765746a6c9e0c
Collodel Umberto <collodelu@centralbankmalta.org>
2025-06-16 12:48:04 +0200

    increased batch size

--- 07running_llm_docs.R
+++ 07running_llm_docs.R
@@ -177,7 +177,7 @@ make_request <- function(text, date, seed = 120) {
 # Run the requests
 
 # Define batch size
-batch_size <- 6
+batch_size <- 10
 
 # Split into batches
 batches <- split(seq_along(ecb_pressconf), ceiling(seq_along(ecb_pressconf) / batch_size))
